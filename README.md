33f9a5139a1f2a8ce51c25f4ec448405992251a0
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-20 19:17:56 +0700

    Update login

--- README.md
+++ README.md
@@ -24,3 +24,26 @@ npm start
 ```
 npm run build
 ```
+
+# Grant database permission
+
+```sql
+create role authenticator noinherit login password 'xxxxxx';
+
+create role myname nologin;
+grant myname to authenticator;
+grant usage on schema public to myname;
+grant all on public.users to myname;
+```
+
+# Create JWT
+
+```json
+{
+  "sub": "1234567890",
+  "name": "John Doe",
+  "iat": 1516239022,
+  "exp": 1674217011,
+  "role": "myname"
+}
+```
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -4,6 +4,8 @@ import {
   getReasonPhrase,
 } from 'http-status-codes';
 
+import * as _ from 'lodash';
+
 import * as crypto from 'crypto'
 
 import { LoginModel } from '../models/login'
@@ -24,43 +26,48 @@ export default async (fastify: FastifyInstance) => {
     },
     schema: loginSchema,
   }, async (request: FastifyRequest, reply: FastifyReply) => {
-    const body: any = request.body
-    const username = body.username
-    const password = body.password
+    const body: any = request.body;
+    const username = body.username;
+    const password = body.password;
 
     try {
-      const encPassword = crypto.createHash('md5').update(password).digest('hex')
-     
-      const { data, error } = await loginModel.login(postgrest, username, encPassword)
-      
+      const encPassword = crypto.createHash('md5').update(password).digest('hex');
+
+      const { data, error } = await loginModel.login(postgrest, username, encPassword);
+
       if (error) {
+        request.log.error(error);
         reply
           .status(StatusCodes.BAD_GATEWAY)
           .send(getReasonPhrase(StatusCodes.BAD_GATEWAY))
       } else {
-        if (data.length > 0) {
-          const user: any = data[0]
-          const payload: any = {
-            sub: user.id
-          }
 
-          const token = fastify.jwt.sign(payload)
+        if (_.isEmpty(data)) {
+          reply
+            .status(StatusCodes.BAD_REQUEST)
+            .send({
+              code: StatusCodes.BAD_REQUEST,
+              error: "Invalid username or password"
+            });
+        } else {
+          const user: any = data[0];
+          const payload: any = { sub: user.id }
+          const token = fastify.jwt.sign(payload);
           reply
             .status(StatusCodes.OK)
-            .send({ access_token: token })
-        } else {
-          reply
-            .status(StatusCodes.UNAUTHORIZED)
-            .send(getReasonPhrase(StatusCodes.UNAUTHORIZED))
+            .send({ access_token: token });
         }
+
       }
 
-      
     } catch (error: any) {
       request.log.error(error);
       reply
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .send(error)
+        .send({
+          code: StatusCodes.INTERNAL_SERVER_ERROR,
+          error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+        });
     }
   })
 
