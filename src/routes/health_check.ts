f4811f04e927c09022a2b1c5ede5a7ca1f4e3e64
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-06 13:19:20 +0700

    update health check response

--- src/routes/health_check.ts
+++ src/routes/health_check.ts
@@ -5,10 +5,12 @@ export default async (fastify: FastifyInstance, _options: any, done: any) => {
 
   fastify.get('/health-check', async (request: FastifyRequest, reply: FastifyReply) => {
     try {
-      reply.status(StatusCodes.OK).send()
+      reply.status(StatusCodes.OK)
+        .send({ status: 'success' })
     } catch (error: any) {
       request.log.error(error)
-      reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send()
+      reply.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ status: 'error' })
     }
   })
 
