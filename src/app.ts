f2ed15c81d20ccb104db44b89d3f7c5914294a7b
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-03 16:01:25 +0700

    Update max connection pool

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.register(require('./plugins/db'), {
     searchPath: [process.env.R7PLATFORM_LOGIN_DB_SCHEMA || 'public'],
     pool: {
       min: Number(process.env.R7PLATFORM_LOGIN_DB_POOL_MIN) || 0,
-      max: Number(process.env.R7PLATFORM_LOGIN_DB_POOL_MAX) || 500
+      max: Number(process.env.R7PLATFORM_LOGIN_DB_POOL_MAX) || 10
     },
     debug: process.env.R7PLATFORM_LOGIN_DB_DEBUG === "Y" ? true : false,
   }
