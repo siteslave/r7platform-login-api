d2a1bc772b3ff7fd26795ece84028e5d33a376f4
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-14 12:01:27 +0700

    add: user info

--- src/routes/health_check.ts
+++ src/routes/health_check.ts
@@ -6,7 +6,11 @@ export default async (fastify: FastifyInstance, _options: any, done: any) => {
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     try {
       reply.status(StatusCodes.OK)
-        .send({ status: 'success' })
+        .send({
+          status: 'ok',
+          version: '1.0.0',
+          name: process.env.R7PLATFORM_LOGIN_HOSTNAME || 'default-server'
+        })
     } catch (error: any) {
       request.log.error(error)
       reply.status(StatusCodes.INTERNAL_SERVER_ERROR)
---
+++ src/routes/info.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
+import { getReasonPhrase, StatusCodes } from "http-status-codes"
+import { InfoModel } from "../models/info"
+
+export default async (fastify: FastifyInstance, _options: any, done: any) => {
+
+  const infoModel = new InfoModel();
+  const db = fastify.db;
+
+  fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+
+      const decoded: any = await request.loginJwtVerify();
+      console.log(decoded);
+
+      const userId: any = decoded.sub;
+
+      if (userId) {
+        const info: any = await infoModel.getInfo(db, userId);
+
+        reply.status(StatusCodes.OK)
+          .send(info);
+      } else {
+        reply.status(StatusCodes.UNAUTHORIZED)
+          .send({ ok: false, error: getReasonPhrase(StatusCodes.UNAUTHORIZED) });
+      }
+    } catch (error: any) {
+      request.log.error(error)
+      reply.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ status: 'error' })
+    }
+  })
+
+  done()
+
+} 
